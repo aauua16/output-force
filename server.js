8fa46f281481fb103f5e5651d2a378ea2b02253f
Fuuga Komada <oaauua18@gmail.com>
2026-06-01 07:30:48 +0900

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const selfsigned = require('selfsigned');
 const app        = express();
 const PORT       = 3000;
 const HTTPS_PORT = 3443;
-const DATA_DIR   = path.join(os.homedir(), 'OneDrive', 'アプリ', 'remotely-save', 'wallfacer');
+const DATA_DIR   = process.env.DATA_DIR;
 const CERT_DIR   = path.join(__dirname, 'cert');
 
 if (!fs.existsSync(DATA_DIR)) fs.mkdirSync(DATA_DIR, { recursive: true });
